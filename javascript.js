f8b7eae758f5d3c33a135057ef3508203bac8e16
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-05-08 15:24:48 +0200

    iteración 7

--- javascript.js
+++ javascript.js
@@ -155,3 +155,25 @@ function finderName(item, i) {
 };
 
 console.log(finderName(nameFinder, 'Peggy',));
+
+// **Iteration #8: Contador de repeticiones**
+
+// Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.  Puedes usar este array para probar tu función:
+
+const counterWords = [
+    'code',
+    'repeat',
+    'eat',
+    'sleep',
+    'code',
+    'enjoy',
+    'sleep',
+    'code',
+    'enjoy',
+    'upgrade',
+    'code'
+];
+
+function repeatCounter(param) {
+    // insert code
+}
