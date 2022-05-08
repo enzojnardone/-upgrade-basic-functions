0347c130cd9d5dd1c1905cb10c26c837c81f1e45
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-05-08 14:31:44 +0200

    iteracion 5

--- javascript.js
+++ javascript.js
@@ -100,3 +100,30 @@ console.log(`El promedio obtenido es ${average(numbers2)}`);
 // **Iteración #6: Valores únicos**
 
 // Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
+
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda',
+];
+
+function removeDuplicates() {
+    const result = [];
+
+    duplicates.forEach((item)=>{
+        if(!result.includes(item)){
+    		result.push(item);
+    	};
+    });  
+    return result
+};
+
+console.log(removeDuplicates(duplicates))
