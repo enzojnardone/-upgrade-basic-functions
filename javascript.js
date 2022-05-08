b88d2374ad965f862e06c8d051c98b878f7ea580
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-05-08 15:12:42 +0200

    iteración 6

--- javascript.js
+++ javascript.js
@@ -117,7 +117,6 @@ const duplicates = [
 
 function removeDuplicates() {
     const result = [];
-
     duplicates.forEach((item)=>{
         if(!result.includes(item)){
     		result.push(item);
@@ -127,3 +126,32 @@ function removeDuplicates() {
 };
 
 console.log(removeDuplicates(duplicates))
+
+// **Iteración #7: Buscador de nombres**
+
+// Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
+
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+];
+
+function finderName(item, i) {
+	for (let name of nameFinder) {
+		if (name === i) {
+			return item.indexOf(name);
+		};
+	};
+	return false;
+};
+
+console.log(finderName(nameFinder, 'Peggy',));
